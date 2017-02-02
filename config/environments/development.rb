353df3f88522fdf08ab9340d3e71119e14d823bc
Gerardo Garcia Sigg <ggsigg@gmail.com>
2017-02-02 14:02:43 -0600

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,10 @@ SampleApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000' # local host
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
 end
