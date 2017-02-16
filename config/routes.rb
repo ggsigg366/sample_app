94691a1421e1267c65aa7dc3d07a52c9079052c2
Gerardo Garcia Sigg <ggsigg@gmail.com>
2017-02-16 10:49:11 -0600

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,16 @@ SampleApp::Application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   
   # The priority is based upon order of creation: first created -> highest priority.
